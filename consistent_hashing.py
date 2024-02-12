5932bc8352f727c5105663c41276603a281548ba
TwistedPair <35606899+rahatbhambri@users.noreply.github.com>
2024-02-12 18:31:36 +0000

    ch changed

--- consistent_hashing.py
+++ consistent_hashing.py
@@ -1,53 +1,63 @@
+import hashlib
+
 class cons_hash():
     def __init__(self, rsize):
         self.ring = [-1 for i in range(rsize)]
         self.hashv = rsize 
+        self.server = {}
+
+    def getHashedPos(self, name):
+        hsh = hashlib.sha256()
+        hsh.update(bytes(name.encode('utf-8')))
+
+        pos = int(hsh.hexdigest(), 16)%self.hashv
+        return pos
     
-    def addServer(self, pos):
-        pos = pos%self.hashv
+    def addServer(self, name):
+        pos = self.getHashedPos(name)
         if self.ring[pos] == -1:
             self.ring[pos] = 1
-            print(f"Server added at {pos}")
+            self.server[pos] = name
+            print(f"Server {name} added at {pos}")
         else:
             print("Server already placed here")
     
-    def removeServer(self, pos):
+    def removeServer(self, name):
+        pos = self.getHashedPos(name)
         if self.ring[pos] == 1:
             self.ring[pos] = 0 
-            print(f"Server removed from {pos}")
+            self.server.pop(pos)
+            print(f"Server {name} removed from {pos}")
         else:
             print("No Server was found")
         
     def serveRequest(self, val):
         init_pos = val%self.hashv
         if self.ring[init_pos] == 1:
-            print(f"request {val} served at {init_pos} server")
+            print(f"request {val} served at {self.server[init_pos]} server")
         else:
             pos = init_pos
             while self.ring[pos%self.hashv] != 1 :
                 pos += 1
-            print(f"request {val} served at {pos%self.hashv} server")
+            print(f"request {val} served at {self.server[pos%self.hashv] } server")
+
+def simulate(consh):
+    for req in range(25, 50):
+        consh.serveRequest(req)
     
+    print("all done")
+
     
 
 consh = cons_hash(16)
 
-consh.addServer(4)
-consh.addServer(8)
-consh.addServer(12)
+consh.addServer("Satyam")
+consh.addServer("Shivama")
+consh.addServer("Sundarama")
 
-for req in range(25, 50):
-    consh.serveRequest(req)
-print("all done")
+consh.removeServer("Shivama")
+simulate(consh)
 
-consh.removeServer(8)
-for req in range(25, 50):
-    consh.serveRequest(req)
-print("all done")
-
-
-consh.addServer(150)
-for req in range(25, 50):
-    consh.serveRequest(req)
-print("all done")
+consh.addServer("Shyama")
+simulate(consh)
 
