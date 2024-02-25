93d5aa92f25afb9712c317d9e40334e639444b4d
TwistedPair <35606899+rahatbhambri@users.noreply.github.com>
2024-02-25 16:54:40 +0530

    Update sql_to_mongo_cdc.py

--- cdc/sql_to_mongo_cdc.py
+++ cdc/sql_to_mongo_cdc.py
@@ -1,5 +1,7 @@
 import pymysql
 from pymongo import MongoClient
+import time
+import pymysqlpool
 
 # MySQL connection settings
 mysql_host = '127.0.0.1'
@@ -8,16 +10,17 @@ mysql_user = 'root'
 mysql_password = ''
 mysql_db = 'sys'
 mysql_table = 'cdc_tracking'
+# Create a connection pool
+pool = pymysqlpool.ConnectionPool(host=mysql_host, port=mysql_port, user=mysql_user,
+                      password=mysql_password, database=mysql_db, name="mypool", 
+                      size=5, autocommit = True)  # Adjust pool_size as needed
+
 
 # MongoDB connection settings
 mongo_uri = 'mongodb://localhost:27017/'
 mongo_db = 'config'
 mongo_collection = 'cdc'
 
-# Connect to MySQL
-mysql_conn = pymysql.connect(host=mysql_host, port=mysql_port, user=mysql_user,
-                             password=mysql_password, database=mysql_db)
-mysql_cursor = mysql_conn.cursor()
 
 # Connect to MongoDB
 mongo_client = MongoClient(mongo_uri)
@@ -27,9 +30,8 @@ mongo_coll = mongo_db[mongo_collection]
 # Function to capture changes from MySQL and write to MongoDB
 def capture_and_write_changes():
     # Execute query to select new changes from MySQL
-    mysql_conn = pymysql.connect(host=mysql_host, port=mysql_port, user=mysql_user,
-                             password=mysql_password, database=mysql_db)
-    mysql_cursor = mysql_conn.cursor()
+    connection = pool.get_connection()
+    mysql_cursor = connection.cursor()
 
     mysql_cursor.execute(f"SELECT * FROM {mysql_table} WHERE status = 'new';")
     new_changes = mysql_cursor.fetchall()
@@ -54,9 +56,10 @@ def capture_and_write_changes():
         # Update status in MySQL to mark changes as processed
         ids = [change[0] for change in new_changes]
         mysql_cursor.executemany("UPDATE {} SET status = 'processed' WHERE id = %s".format(mysql_table), ids)
-        mysql_conn.commit()
+        #mysql_conn.commit()
 
 # Main loop to continuously capture and write changes
 while True:
     capture_and_write_changes()
+    time.sleep(2)
     # Add delay or use triggers/events to capture changes more efficiently
